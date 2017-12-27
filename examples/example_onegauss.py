645c2862e35815a90d71bd1a205f0020bb7f491f
Tobias Kohn <webmaster@tobiaskohn.ch>
2017-12-27 11:23:43 +0100

    Added an example.

---
+++ examples/example_onegauss.py
@@ -0,0 +1,10 @@
+"""
+A simple example of FOPPL in Python.
+
+After importing FOPPL, we display the entire documentation for the
+generated model.
+"""
+from foppl import imports
+import onegauss
+
+print(help(test_onegauss.model))
